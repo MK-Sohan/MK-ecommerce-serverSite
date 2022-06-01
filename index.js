20b5a11661422d00e98772096a4ce8bb06e8f8e7
MK-Sohan <mksohan786@gmail.com>
2022-06-01 14:33:09 +0600

    delete api

--- index.js
+++ index.js
@@ -120,6 +120,12 @@ async function run() {
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
