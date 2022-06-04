5966400c0f6d4106fbc0b7631099d50ea6f893d4
MK-Sohan <mksohan786@gmail.com>
2022-06-04 20:47:47 +0600

    admin

--- index.js
+++ index.js
@@ -23,9 +23,12 @@ function varifyJwt(req, res, next) {
     res.status(401).send({ message: "Un authorize access" });
   }
   const token = authHeader.split(" ")[1];
-  jwt.verify(token, "shhhhh", function (err, decoded) {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
     req.decoded = decoded;
-    // console.log(decoded);
+    // console.log("this is decoded", decoded);
     next();
   });
 }
@@ -59,7 +62,7 @@ async function run() {
       const result = await allProductsCollection.find(query).toArray();
       res.send(result);
     });
-    app.get("/shoes", async (req, res) => {
+    app.get("/shoes", varifyJwt, async (req, res) => {
       const catagory = req.query.catagory;
       const query = { catagory: catagory };
       const result = await allProductsCollection.find(query).toArray();
@@ -76,7 +79,7 @@ async function run() {
       const result = await allProductsCollection.find(query).toArray();
       res.send(result);
     });
-    app.get("/reviews", async (req, res) => {
+    app.get("/reviews", varifyJwt, async (req, res) => {
       const query = {};
       const result = await reviewsCollection.find(query).toArray();
       res.send(result);
@@ -90,7 +93,7 @@ async function run() {
       res.send(result);
     });
 
-    app.put("/user/:email", async (req, res) => {
+    app.put("/user/:email", varifyJwt, async (req, res) => {
       const email = req.params.email;
       const user = req.body;
       const filter = { email: email };
@@ -104,15 +107,49 @@ async function run() {
       });
       res.send({ result, token });
     });
+    app.put("/user/admin/:email", varifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "Forbidden Accrss" });
+      }
+    });
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user?.role === "admin";
+      res.send({ admin: isAdmin });
+    });
 
     app.get("/user", async (req, res) => {
       const query = {};
       const result = await userCollection.find(query).toArray();
       res.send(result);
     });
-    app.post("/cart", varifyJwt, async (req, res) => {
-      const cart = req.body;
-      const result = await cartCollection.insertOne(cart);
+    app.put("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const cartProduct = req.body;
+      const filter = { _id: id };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: cartProduct,
+      };
+      const result = await cartCollection.updateOne(filter, updateDoc, options);
+      // const cart = req.body;
+      // const result = await cartCollection.insertOne(cart);
       res.send(result);
     });
     app.get("/carts/:email", async (req, res) => {
@@ -124,7 +161,7 @@ async function run() {
     });
     app.delete("/cart/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId(id) };
+      const query = { _id: id };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
@@ -143,15 +180,21 @@ async function run() {
       );
       res.send(result);
     });
-    app.get("/updateprofile/:email", varifyJwt, async (req, res) => {
+    app.get("/updateprofile/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await profileCollection.findOne(query);
       res.send(result);
     });
-    app.get("/alluser", async (req, res) => {
+    app.get("/alluser", varifyJwt, async (req, res) => {
       const query = {};
       const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/userdelete/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
